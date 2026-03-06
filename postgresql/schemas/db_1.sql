265235e90ae72438b3499975f55e903da395e9f3
453254 <gradmir7@gmail.com>
2026-03-06 16:07:00 +0300

    fully implemented DB

--- postgresql/schemas/db_1.sql
+++ postgresql/schemas/db_1.sql
@@ -7,19 +7,33 @@ CREATE TABLE IF NOT EXISTS seagull_schema.users (
     login VARCHAR(50) UNIQUE NOT NULL,
     password_hash TEXT NOT NULL,
     salt TEXT NOT NULL,
-    name VARCHAR(100) NOT NULL CHECK (char_length(btrim(name)) > 0)
+    name VARCHAR(100) NOT NULL,
+    bio TEXT
 );
 
 CREATE TABLE IF NOT EXISTS seagull_schema.messages (
     message_id SERIAL PRIMARY KEY,
     content TEXT NOT NULL,
     is_read BOOLEAN DEFAULT FALSE,
-    sent_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+    send_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS seagull_schema.actions (
     action_id SERIAL PRIMARY KEY,
     sender_id INT REFERENCES seagull_schema.users(user_id) ON DELETE CASCADE,
     message_id INT REFERENCES seagull_schema.messages(message_id) ON DELETE CASCADE,
-    chat_id INT DEFAULT 0
+    chat_id INT REFERENCES seagull_schema.chats(chat_id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS seagull_schema.chats (
+    chat_id SERIAL PRIMARY KEY,
+    chat_name VARCHAR(100) NOT NULL,
+    is_private BOOLEAN NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS seagull_schema.users_chats (
+    chat_id INT REFERENCES seagull_schema.chats(chat_id) ON DELETE CASCADE,
+    user_id INT REFERENCES seagull_schema.users(user_id) ON DELETE CASCADE,
+    joined_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (chat_id, user_id)
 );
